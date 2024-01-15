03c5621847824d6211e628bcf84d43258d73f8d0
chen.siyuan <ischen.siyuan@foxmail.com>
2024-01-15 14:45:10 +0800

    Update

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -108,77 +108,56 @@ G4VPhysicalVolume* DetectorConstruction::Construct() {
     // lyso->AddElement(lutetiumElement, 0.730562);
     // lyso->AddElement(cesiumElement, 0.012379);
 
-    /////////////////////////////////////////////
-    // Construct Material Properties Tables
-    /////////////////////////////////////////////
+    //////////////////////////////////////////////////
+    // Construct Material Optical Properties Tables
+    //////////////////////////////////////////////////
 
-    const auto fLambda_min = 200 * nm;
-    const auto fLambda_max = 700 * nm;
-    std::vector<G4double> ENERGY_BIN = {h_Planck * c_light / fLambda_max, h_Planck * c_light / fLambda_min};
+    constexpr auto fLambda_min = 200 * nm;
+    constexpr auto fLambda_max = 700 * nm;
+    std::vector<G4double> energyPair = {h_Planck * c_light / fLambda_max,
+                                        h_Planck * c_light / fLambda_min};
 
     //============================================ air ================================================
 
-    std::vector<G4double> RINDEX_AIR = {1.00, 1.00};
-    // Air refractive index at 20 oC and 1 atm (from PDG)
-    for (auto&& i : RINDEX_AIR) {
-        i = i + 2.73 * std::pow(10.0, -4);
-    }
-
     auto MPT_Air = new G4MaterialPropertiesTable();
-    MPT_Air->AddProperty("RINDEX", ENERGY_BIN, RINDEX_AIR);
+    MPT_Air->AddProperty("RINDEX", energyPair, {1.00, 1.00});
     galactic->SetMaterialPropertiesTable(MPT_Air);
 
     //============================================ silicone oil =======================================
 
-    // silicone oil refractive index
-    std::vector<G4double> ENERGY_SiOIL = {8.01532E-07, 1.89386E-06, 1.92915E-06, 2.1093E-06, 2.27541E-06, 2.55633E-06, 2.58828E-06};
-    std::vector<G4double> RINDEX_SiOIL = {1.3912, 1.3992, 1.3997, 1.4015, 1.4034, 1.4071, 1.4076};
-
-    // silicone oil absoption length
-    std::vector<G4double> ABSL_SiOIL = {15 * cm, 15 * cm};
+    std::vector<G4double> couplerEnergyBin = {8.01532E-07, 1.89386E-06, 1.92915E-06, 2.1093E-06, 2.27541E-06, 2.55633E-06, 2.58828E-06};
+    std::vector<G4double> couplerRefractiveIndex = {1.3912, 1.3992, 1.3997, 1.4015, 1.4034, 1.4071, 1.4076};
 
     auto siliconeOilPropertiesTable = new G4MaterialPropertiesTable();
-    siliconeOilPropertiesTable->AddProperty("RINDEX", ENERGY_SiOIL, RINDEX_SiOIL);
-    siliconeOilPropertiesTable->AddProperty("ABSLENGTH", ENERGY_BIN, ABSL_SiOIL);
+    siliconeOilPropertiesTable->AddProperty("RINDEX", couplerEnergyBin, couplerRefractiveIndex);
+    siliconeOilPropertiesTable->AddProperty("ABSLENGTH", energyPair, {15 * cm, 15 * cm});
     siliconeOil->SetMaterialPropertiesTable(siliconeOilPropertiesTable);
 
     //============================================ Quartz =============================================
 
-    std::vector<G4double> RINDEX_QUARTZ = {1.54, 1.54};
-
     auto windowPropertiesTable = new G4MaterialPropertiesTable();
-    windowPropertiesTable->AddProperty("RINDEX", ENERGY_BIN, RINDEX_QUARTZ);
+    windowPropertiesTable->AddProperty("RINDEX", energyPair, {1.54, 1.54});
     glass->SetMaterialPropertiesTable(windowPropertiesTable);
-    windowPropertiesTable->DumpTable();
 
     //============================================ Crystal ============================================
 
     // CsI(Tl)
-    std::vector<G4double> GROUPV_CSI = {167.482, 167.482};
 
     auto csiPropertiesTable = new G4MaterialPropertiesTable;
     auto csiProperties(CreateMapFromCSV<G4double>("data/CsI_properties.csv"));
-    csiPropertiesTable->AddProperty(
-        "RINDEX",
-        csiProperties["energy"],
-        csiProperties["RINDEX"]);
-    csiPropertiesTable->AddProperty(
-        "GROUPVEL",
-        ENERGY_BIN,
-        GROUPV_CSI);
-    csiPropertiesTable->AddProperty(
-        "ABSLENGTH",
-        csiProperties["energy"],
-        csiProperties["ABSLENGTH"]);
-    csiPropertiesTable->AddProperty(
-        "SCINTILLATIONCOMPONENT1",
-        csiProperties["energy"],
-        csiProperties["SCINTILLATIONCOMPONENT1"]);
-    csiPropertiesTable->AddConstProperty("SCINTILLATIONYIELD", csiProperties["SCINTILLATIONYIELD"].front());
-    csiPropertiesTable->AddConstProperty("SCINTILLATIONTIMECONSTANT1", csiProperties["SCINTILLATIONTIMECONSTANT1"].front());
-    csiPropertiesTable->AddConstProperty("RESOLUTIONSCALE", csiProperties["RESOLUTIONSCALE"].front());
+    csiPropertiesTable->AddProperty("RINDEX", csiProperties["energy"], csiProperties["RINDEX"]);
+    csiPropertiesTable->AddProperty("GROUPVEL", energyPair, {167.482, 167.482});
+    csiPropertiesTable->AddProperty("ABSLENGTH", csiProperties["energy"], csiProperties["ABSLENGTH"]);
+    csiPropertiesTable->AddProperty("SCINTILLATIONCOMPONENT1",
+                                    csiProperties["energy"],
+                                    csiProperties["SCINTILLATIONCOMPONENT1"]);
+    csiPropertiesTable->AddConstProperty("SCINTILLATIONYIELD",
+                                         csiProperties["SCINTILLATIONYIELD"].front());
+    csiPropertiesTable->AddConstProperty("SCINTILLATIONTIMECONSTANT1",
+                                         csiProperties["SCINTILLATIONTIMECONSTANT1"].front());
+    csiPropertiesTable->AddConstProperty("RESOLUTIONSCALE",
+                                         csiProperties["RESOLUTIONSCALE"].front());
     csI->SetMaterialPropertiesTable(csiPropertiesTable);
-    csiPropertiesTable->DumpTable();
 
     // // LaBr3(Ce)
     // auto labrPropertiesTable = new G4MaterialPropertiesTable;
@@ -220,7 +199,41 @@ G4VPhysicalVolume* DetectorConstruction::Construct() {
     // lysoPropertiesTable->AddConstProperty("RESOLUTIONSCALE", lysoProperties["RESOLUTIONSCALE"].front());
     // lyso->SetMaterialPropertiesTable(lysoPropertiesTable);
 
-    // Define World Volume
+    auto rfSurfacePropertiesTable = new G4MaterialPropertiesTable();
+    rfSurfacePropertiesTable->AddProperty("REFLECTIVITY", energyPair, {0.985, 0.985});
+
+    std::vector<G4double> pmtWaveLengthBin = {715.759, 704.541, 687.714, 670.887, 654.06, 637.234, 620.807, 606.384,
+                                              592.562, 584.019, 577.939, 571.814, 566.671, 562.542, 558.307, 553.099,
+                                              547.49, 541.48, 534.669, 527.057, 519.361, 511.903, 505.422, 499.413,
+                                              493.804, 487.821, 481.651, 473.856, 465.246, 456.513, 443.724, 427.297,
+                                              410.47, 393.643, 376.816, 359.989, 347.57, 341.159, 335.766, 332.51,
+                                              328.633, 325.763, 323.291, 320.44, 318.552, 316.506, 313.615, 312.091,
+                                              309.509, 307.334, 305.549, 302.698, 301.596, 299.342, 296.555, 294.161,
+                                              291.88, 288.847, 285.737, 281.108};
+    std::vector<G4double> pmtQuantumEfficiency = {0.206, 0.237, 0.282, 0.398, 0.74, 1.321, 2.113, 3.024,
+                                                  3.969, 4.905, 5.856, 6.819, 7.723, 8.636, 9.57, 10.508,
+                                                  11.467, 12.374, 13.281, 14.205, 15.191, 16.195, 17.195,
+                                                  18.114, 18.987, 19.886, 20.83, 21.794, 22.8, 23.806, 24.644,
+                                                  25.312, 25.713, 25.932, 25.835, 25.279, 24.266, 23.367, 22.357,
+                                                  21.43, 20.344, 19.319, 18.363, 17.294, 16.265, 15.232, 14.053,
+                                                  12.759, 11.486, 10.345, 9.229, 8.193, 7.198, 6.108, 5.136, 4.241,
+                                                  3.37, 2.403, 1.447, 0.466};
+
+    std::vector<G4double> cathodeSurfacePropertiesEnergy(pmtWaveLengthBin.size());
+    std::vector<G4double> cathodeSurfacePropertiesEfficiency(pmtQuantumEfficiency.size());
+
+    std::transform(pmtWaveLengthBin.begin(), pmtWaveLengthBin.end(), cathodeSurfacePropertiesEnergy.begin(),
+                   [](auto val) { return h_Planck * c_light / (val * nm / mm); });
+    std::transform(pmtQuantumEfficiency.begin(), pmtQuantumEfficiency.end(), cathodeSurfacePropertiesEfficiency.begin(),
+                   [](auto n) { return n * perCent; });
+
+    const auto cathodeSurfacePropertiesTable = new G4MaterialPropertiesTable();
+    cathodeSurfacePropertiesTable->AddProperty("REFLECTIVITY", energyPair, {0., 0.});
+    cathodeSurfacePropertiesTable->AddProperty("EFFICIENCY", cathodeSurfacePropertiesEnergy, cathodeSurfacePropertiesEfficiency);
+
+    /////////////////////////////////////////////
+    // Construct Volumes
+    /////////////////////////////////////////////
 
     const auto worldSize = 2 * m;
     const auto worldS = new G4Box("world", 0.5 * worldSize, 0.5 * worldSize, 0.5 * worldSize);
@@ -231,7 +244,7 @@ G4VPhysicalVolume* DetectorConstruction::Construct() {
     const auto fCrystalHalfLength = 7.5 * cm;
 
     const auto fCouplerHalfWidth = 35 * mm;
-    const auto fCouplerHalfThickness = 200 * um;
+    const auto fCouplerHalfThickness = 0.05 * mm;
 
     const auto fPMTWindowDiameter = fCouplerHalfWidth;
     const auto fPMTWindowHalfThickness = 0.5 * mm;
@@ -258,7 +271,7 @@ G4VPhysicalVolume* DetectorConstruction::Construct() {
     }
 
     G4TwoVector offsetA(0, 0), offsetB(0, offsetlength);
-    G4double scaleA = 0.5, scaleB = 1;
+    G4double scaleA = 0.6, scaleB = 1;
 
     const auto crystalSV = new G4ExtrudedSolid("Extruded", polygon, fCrystalHalfLength, offsetA, scaleA, offsetB, scaleB);
 
@@ -289,46 +302,46 @@ G4VPhysicalVolume* DetectorConstruction::Construct() {
 
     // Define Surface
 
-    std::vector<G4double> REFLECTIVITY = {0.955, 0.955};
-    auto tempPropertiesTable = new G4MaterialPropertiesTable();
-    tempPropertiesTable->AddProperty("REFLECTIVITY", ENERGY_BIN, REFLECTIVITY);
-
-    auto alSurface = new G4OpticalSurface("Al foil", glisur, ground, dielectric_metal);
+    auto alSurface = new G4OpticalSurface("Al foil", unified, ground, dielectric_metal);
     new G4LogicalBorderSurface("AlSkinSurface", crystalPV, worldPV, alSurface);
-    alSurface->SetMaterialPropertiesTable(tempPropertiesTable);
+    alSurface->SetMaterialPropertiesTable(rfSurfacePropertiesTable);
 
-    auto optocouplerSurface = new G4OpticalSurface("Optocoupler", glisur, polished, dielectric_metal);
+    auto optocouplerSurface = new G4OpticalSurface("Optocoupler", unified, polished, dielectric_metal);
     new G4LogicalBorderSurface("optocouplerSurface", couplerPV, worldPV, optocouplerSurface);
-    optocouplerSurface->SetMaterialPropertiesTable(tempPropertiesTable);
+    optocouplerSurface->SetMaterialPropertiesTable(rfSurfacePropertiesTable);
 
-    auto windowSurface = new G4OpticalSurface("Optocoupler", glisur, polished, dielectric_metal);
+    auto windowSurface = new G4OpticalSurface("Optocoupler", unified, polished, dielectric_metal);
     new G4LogicalBorderSurface("windowSurface", windowPV, worldPV, windowSurface);
-    windowSurface->SetMaterialPropertiesTable(tempPropertiesTable);
+    windowSurface->SetMaterialPropertiesTable(rfSurfacePropertiesTable);
 
-    auto cathodeSurfacePropertiesTable = new G4MaterialPropertiesTable();
-    auto cathodeSurfaceProperties(CreateMapFromCSV<G4double>("data/PMT_properties.csv"));
-
-    std::vector<G4double> cathodeSurfacePropertiesEnergy(cathodeSurfaceProperties["wavelength"].size());
-    std::vector<G4double> cathodeSurfacePropertiesEfficiency(cathodeSurfaceProperties["EFFICIENCY"].size());
-
-    std::transform(cathodeSurfaceProperties["wavelength"].begin(), cathodeSurfaceProperties["wavelength"].end(), cathodeSurfacePropertiesEnergy.begin(),
-                   [](auto val) { return h_Planck * c_light / (val * nm / mm); });
-
-    std::transform(cathodeSurfaceProperties["EFFICIENCY"].begin(), cathodeSurfaceProperties["EFFICIENCY"].end(), cathodeSurfacePropertiesEfficiency.begin(),
-                   [](auto n) { return n * perCent; });
-
-    auto cathodeSurface = new G4OpticalSurface("Cathode", glisur, polished, dielectric_metal);
-    new G4LogicalSkinSurface("sipmSkinSurface", sipmLV, cathodeSurface);
-    cathodeSurfacePropertiesTable->AddProperty(
-        "REFLECTIVITY",
-        cathodeSurfacePropertiesEnergy,
-        cathodeSurfaceProperties["REFLECTIVITY"]);
-    cathodeSurfacePropertiesTable->AddProperty(
-        "EFFICIENCY",
-        cathodeSurfacePropertiesEnergy,
-        cathodeSurfacePropertiesEfficiency);
+    const auto cathodeSurface = new G4OpticalSurface("Cathode", unified, polished, dielectric_metal);
+    new G4LogicalSkinSurface("cathodeSkinSurface", sipmLV, cathodeSurface);
     cathodeSurface->SetMaterialPropertiesTable(cathodeSurfacePropertiesTable);
-    cathodeSurfacePropertiesTable->DumpTable();
+
+    // auto cathodeSurfacePropertiesTable = new G4MaterialPropertiesTable();
+    // auto cathodeSurfaceProperties(CreateMapFromCSV<G4double>("data/PMT_properties.csv"));
+
+    // std::vector<G4double> cathodeSurfacePropertiesEnergy(cathodeSurfaceProperties["wavelength"].size());
+    // std::vector<G4double> cathodeSurfacePropertiesEfficiency(cathodeSurfaceProperties["EFFICIENCY"].size());
+
+    // std::transform(cathodeSurfaceProperties["wavelength"].begin(), cathodeSurfaceProperties["wavelength"].end(), cathodeSurfacePropertiesEnergy.begin(),
+    //                [](auto val) { return h_Planck * c_light / (val * nm / mm); });
+
+    // std::transform(cathodeSurfaceProperties["EFFICIENCY"].begin(), cathodeSurfaceProperties["EFFICIENCY"].end(), cathodeSurfacePropertiesEfficiency.begin(),
+    //                [](auto n) { return n * perCent; });
+
+    // auto cathodeSurface = new G4OpticalSurface("Cathode", glisur, polished, dielectric_metal);
+    // new G4LogicalSkinSurface("sipmSkinSurface", sipmLV, cathodeSurface);
+    // cathodeSurfacePropertiesTable->AddProperty(
+    //     "REFLECTIVITY",
+    //     cathodeSurfacePropertiesEnergy,
+    //     cathodeSurfaceProperties["REFLECTIVITY"]);
+    // cathodeSurfacePropertiesTable->AddProperty(
+    //     "EFFICIENCY",
+    //     cathodeSurfacePropertiesEnergy,
+    //     cathodeSurfacePropertiesEfficiency);
+    // cathodeSurface->SetMaterialPropertiesTable(cathodeSurfacePropertiesTable);
+    // cathodeSurfacePropertiesTable->DumpTable();
 
     ++cellNumber;
 
